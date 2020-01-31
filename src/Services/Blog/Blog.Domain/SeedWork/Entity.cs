f3a3e0e892055921c7ba3a1a14df7b7ec55b43f6
Shukurdin <ShukurdinAi@halykban.kz>
2020-01-31 17:44:50 +0600

    Seed work

---
+++ src/Services/Blog/Blog.Domain/SeedWork/Entity.cs
@@ -0,0 +1,73 @@
+﻿namespace Blog.Domain
+{
+    using MediatR;
+    using System.Collections.Generic;
+
+    public abstract class Entity
+    {
+        public virtual int Id { get; protected set; }
+
+        private readonly List<INotification> events = new List<INotification>();
+        public IReadOnlyCollection<INotification> Events => events.AsReadOnly();
+
+        public void AddEvent(INotification e)
+        {
+            events.Add(e);
+        }
+
+        public void RemoveEvent(INotification e)
+        {
+            events.Remove(e);
+        }
+
+        public void ClearEvents()
+        {
+            events.Clear();
+        }
+
+        public bool IsTransient()
+        {
+            return Id == default;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Entity))
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            var entity = (Entity)obj;
+
+            return entity.IsTransient() || IsTransient()
+                ? false
+                : entity.Id == Id;
+        }
+
+        private int? requestedHashCode;
+        public override int GetHashCode()
+        {
+            if (IsTransient())
+                return base.GetHashCode();
+
+            if (!requestedHashCode.HasValue)
+                requestedHashCode = Id.GetHashCode() ^ 31; // XOR for random distribution (http://blogs.msdn.com/b/ericlippert/archive/2011/02/28/guidelines-and-rules-for-gethashcode.aspx)
+
+            return requestedHashCode.Value;
+        }
+
+        public static bool operator ==(Entity left, Entity right)
+        {
+            return Equals(left, null) ? Equals(right, null) : left.Equals(right);
+        }
+
+        public static bool operator !=(Entity left, Entity right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ src/Services/Blog/Blog.Domain/SeedWork/Enumeration.cs
@@ -0,0 +1,31 @@
+﻿namespace Blog.Domain.SeedWork
+{
+    using System;
+    using System.Collections;
+
+    public abstract class Enumeration : IComparable
+    {
+        protected Enumeration(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; }
+
+        public string Name { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Enumeration other))
+                return false;
+
+            var typeEquals = GetType().Equals(obj.GetType());
+            var valueEquals = Id.Equals(other.Id);
+
+            return typeEquals && valueEquals;
+        }
+
+        public int CompareTo(object other) => Id.CompareTo(((Enumeration)other).Id);
+    }
+}
